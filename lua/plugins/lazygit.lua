d692920b8ff825ae3495f5e48ef9980094bb73eb
Bamdad Sabbagh <hi@bamdad.fr>
2023-10-17 14:46:39 +0200

    Add light version

--- lua/plugins/lazygit.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "kdheepak/lazygit.nvim",
-  keys = {
-    { "<leader>ç", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-    -- { "<A-ç>", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-  },
-}
--- lua/plugins/todo.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "folke/todo-comments.nvim",
-  keys = {
-    { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find TODO/FIX/FIXME" },
-    { "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "Find all TODOs" },
-  },
-}
